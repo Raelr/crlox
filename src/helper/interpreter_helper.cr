6219d2da3933ba32c8c8fcf788191c0ce09a488d
Aryeh <aryehzinn@hotmail.com>
2021-06-25 11:58:43 +1000

    Refactored and cleaned up interpreter code

---
+++ src/helper/interpreter_helper.cr
@@ -0,0 +1,41 @@
+require "../interpreter/interpreter_errors"
+require "../token/*"
+
+module CrLox::Helper
+  def stringify(object : LiteralType)
+    return "nil" if object == nil
+    if object.is_a?(Float64)
+      return format_number(object).to_s
+    end
+    object.to_s
+  end
+
+  def format_number(number : Float64)
+    number.to_i == number ? number.to_i : number
+  end
+
+  def equal?(left : LiteralType, right : LiteralType) : Bool
+    return true if left == nil && right == nil
+    return false if left == nil
+    left == right
+  end
+
+  def truthy?(literal : LiteralType) : Bool
+    return literal.as(Bool) if literal.is_a?(Bool)
+    return !(literal == nil)
+  end
+
+  def verify_number_operand(operator : Token, operand : LiteralType)
+    return if operand.is_a?(Float64)
+    log_runtime_error(operator, "Operand must be a number.")
+  end
+
+  def verify_number_operands(operator : Token, left : LiteralType, right : LiteralType)
+    return if left.is_a?(Float64) && right.is_a?(Float64)
+    log_runtime_error(operator, "Operands must be numbers.")
+  end
+
+  def log_runtime_error(operator : Token, message : String)
+    raise(RuntimeException.new(operator, message))
+  end
+end
--- src/interpreter/interpreter.cr
+++ src/interpreter/interpreter.cr
@@ -2,6 +2,7 @@ require "../tool/expr"
 require "../token/*"
 require "../helper/error_helper"
 require "./interpreter_errors.cr"
+require "../helper/interpreter_helper"
 
 module CrLox
   extend CrLox
@@ -16,6 +17,7 @@ module CrLox
       begin
         result = evaluate(expression)
       rescue ex : RuntimeException
+        @had_error = true
         @error_message += "#{runtime_error(ex)}"
       end
 
@@ -29,47 +31,40 @@ module CrLox
 
       case expr.operator.type
       when TokenType::GREATER
-        check_number_operands(expr.operator, left, right)
+        verify_number_operands(expr.operator, left, right)
         return left.as(Float64) > right.as(Float64)
       when TokenType::GREATER_EQUAL
-        check_number_operands(expr.operator, left, right)
+        verify_number_operands(expr.operator, left, right)
         return left.as(Float64) >= left.as(Float64)
       when TokenType::LESS
-        check_number_operands(expr.operator, left, right)
+        verify_number_operands(expr.operator, left, right)
         return left.as(Float64) < right.as(Float64)
       when TokenType::LESS_EQUAL
-        check_number_operands(expr.operator, left, right)
+        verify_number_operands(expr.operator, left, right)
         return left.as(Float64) <= right.as(Float64)
       when TokenType::BANG_EQUAL ; return !equal?(left, right)
       when TokenType::EQUAL_EQUAL; return equal?(left, right)
       when TokenType::MINUS
-        check_number_operands(expr.operator, left, right)
+        verify_number_operands(expr.operator, left, right)
         return left.as(Float64) - right.as(Float64)
       when TokenType::SLASH
-        check_number_operands(expr.operator, left, right)
+        verify_number_operands(expr.operator, left, right)
+        log_runtime_error(expr.operator, "Cannot divide value by 0!") if right == 0
         return left.as(Float64) / right.as(Float64)
       when TokenType::STAR
-        check_number_operands(expr.operator, left, right)
+        verify_number_operands(expr.operator, left, right)
         return left.as(Float64) * right.as(Float64)
       when TokenType::PLUS
         if left.is_a?(Float64) && right.is_a?(Float64)
           return left.as(Float64) + right.as(Float64)
         end
-        if left.is_a?(String) && right.is_a?(String)
-          return "#{left}#{right}"
+        if (left.is_a?(String) && right.is_a?(String)) || left.is_a?(String) && right.is_a?(Float64)
+          return "#{left}#{right.is_a?(Float64) ? format_number(right) : right}"
         end
         log_runtime_error(expr.operator, "Operands must be two numbers or two strings.")
       end
 
       nil
-    end
-
-    def stringify(object : LiteralType)
-      return "nil" if object == nil
-      if object.is_a?(Float64)
-        return object.to_s
-      end
-      object.to_s
     end
 
     def visit_grouping_expr(expr : Grouping) : LiteralType
@@ -84,41 +79,17 @@ module CrLox
       right = evaluate(expr.right)
 
       case expr.operator.type
-      when TokenType::MINUS; return -right.as(Float64)
-      when TokenType::BANG ; return !is_truthy?(right)
+      when TokenType::MINUS
+        verify_number_operand(expr.operator, right)
+        return -right.as(Float64)
+      when TokenType::BANG; return !truthy?(right)
       end
 
       nil
     end
 
-    def check_number_operand(operator : Token, operand : LiteralType)
-      return if operand.is_a?(Float64)
-      log_runtime_error(operator, "Operand must be a number.")
-    end
-
-    def check_number_operands(operator : Token, left : LiteralType, right : LiteralType)
-      return if left.is_a?(Float64) && right.is_a?(Float64)
-      log_runtime_error(operator, "Operands must be numbers.")
-    end
-
-    def is_truthy?(literal : LiteralType) : Bool
-      return literal.as(Bool) if literal.is_a?(Bool)
-      return !(literal == nil)
-    end
-
-    def equal?(left : LiteralType, right : LiteralType) : Bool
-      return true if left == nil && right == nil
-      return false if left == nil
-      left == right
-    end
-
     def evaluate(expr : Expr) : LiteralType
       expr.accept(self)
-    end
-
-    def log_runtime_error(operator : Token, message : String)
-      @had_error = true
-      raise(RuntimeException.new(operator, message))
     end
   end
 end
